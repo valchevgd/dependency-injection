0544ceea1fd67d77ca078088244fba9e59d7881d
valchevgd <valchevgd@gmail.com>
2020-05-09 08:15:34 +0300

    Default Profile Added

--- src/main/java/com/github/valchevgd/dependencyinjection/services/I18nBulgarianService.java
+++ src/main/java/com/github/valchevgd/dependencyinjection/services/I18nBulgarianService.java
@@ -3,7 +3,7 @@ package com.github.valchevgd.dependencyinjection.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("BG")
+@Profile({ "BG", "default" })
 @Service("i18nService")
 public class I18nBulgarianService implements GreetingService {
     @Override
